669779b5fe21796b082b91f016d84f7047ef243a
Justin Wit <jwit3@gatech.edu>
2025-02-07 13:26:45 -0500

    define real_pick_coke task configs

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import pathlib
 
 ### Task parameters
-DATA_DIR = '<put your data dir here>'
+DATA_DIR = '/data3/act_data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
@@ -29,6 +29,16 @@ SIM_TASK_CONFIGS = {
         'num_episodes': 50,
         'episode_len': 500,
         'camera_names': ['top']
+    },
+}
+
+
+TASK_CONFIGS = {
+    'real_pick_coke':{
+        'dataset_dir': DATA_DIR + '/real_pick_coke',
+        'num_episodes': 40,
+        'episode_len': 100,
+        'camera_names': ['rgb_frames']
     },
 }
 
