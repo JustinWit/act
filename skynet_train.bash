26ae8a71258696b45209ebc22a471f9d2b7cfd40
Jeremiah Coholich <jmcoholich@gmail.com>
2025-02-11 15:51:22 -0500

    fix but in skynet train script

--- skynet_train.bash
+++ skynet_train.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
-# SBATCH --job-name=act_training
-# SBATCH -p kira-lab
-# SBATCH -G 2080_ti:1
-# SBATCH -c 7
-# SBATCH --qos=short
+#SBATCH --job-name=act_training
+#SBATCH -p kira-lab
+#SBATCH -G 2080_ti:1
+#SBATCH -c 7
+#SBATCH --qos=short
+#SBATCH -x hal,friday
 
 set -ex
+nvidia-smi
+
 USER=$(whoami)
 source /coc/testnvme/$USER/.bashrc
 conda activate aloha
