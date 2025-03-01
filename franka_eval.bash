424965fe82d1f37b7f4b9dab0fd63baab8cd1c7a
Justin Wit <jwit3@gatech.edu>
2025-03-01 12:32:12 -0500

    analyze sim v real

--- franka_eval.bash
+++ franka_eval.bash
@@ -5,11 +5,12 @@ set -ex
 python imitate_episodes.py \
 --eval \
 --ckpt_dir act_models/$1 \
---policy_class ACT --kl_weight 10 --chunk_size 5 --hidden_dim 512 --batch_size 128 --dim_feedforward 3200 \
+--policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 128 --dim_feedforward 3200 \
 --num_epochs 20000 --lr 1e-4 \
 --seed 0 \
---ckpt_name $2 \
+--ckpt_name policy_epoch_8000_seed_0.ckpt \
 --run_name eval \
---absolute_actions \
 --no_proprioception \
+--full_size_img \
+--absolute_actions \
 --temporal_agg \
