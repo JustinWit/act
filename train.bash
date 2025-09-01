2e419575b633a7c1aae61447c6a0600c3a9ad8ec
Jeremiah Coholich <jmcoholich@gmail.com>
2025-09-01 17:27:44 -0400

    update default training bash

--- train.bash
+++ train.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ex
 
-python -m pdb imitate_episodes.py \
+python imitate_episodes.py \
 --task_name $1 \
 --ckpt_dir act_models/$2 \
---policy_class ACT --kl_weight 10 --chunk_size 10 --hidden_dim 512 --batch_size 50 --dim_feedforward 3200 \
---num_epochs 20000 --lr 5e-5 \
+--policy_class ACT --kl_weight 10 --chunk_size 20 --hidden_dim 512 --batch_size 100 --dim_feedforward 3200 \
+--num_epochs 10000 --lr 5e-5 \
 --seed 0 \
 --run_name $2 \
 --absolute_actions \
---no_wandb \
+# --no_wandb \
 # --no_proprioception \
 # --preload_to_gpu \
 # --full_size_img \
